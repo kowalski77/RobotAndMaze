7266e73c37de8e206b0195dca4cee1131eb8c4f7
Christian Aranda <christian.aranda@contractors.roche.com>
2021-06-29 14:15:20 +0200

    refactor switch/case in RobotMachoneProvider

--- RobotAndMaze/Domain/Factories/RobotMachineProvider.cs
+++ RobotAndMaze/Domain/Factories/RobotMachineProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using RobotAndMaze.Domain.Models;
+﻿using RobotAndMaze.Domain.Models;
 using RobotAndMaze.Support;
 
 namespace RobotAndMaze.Domain.Factories
@@ -18,12 +17,12 @@ namespace RobotAndMaze.Domain.Factories
                 Direction.Back => matrix.CheckCoordinates(coordinates.XPos, coordinates.YPos - this.robot.Back.Value),
                 Direction.Left => matrix.CheckCoordinates(coordinates.XPos - this.robot.Left.Value, coordinates.YPos),
                 Direction.Right => matrix.CheckCoordinates(coordinates.XPos + this.robot.Right.Value, coordinates.YPos),
-                _ => throw new ArgumentOutOfRangeException($"{nameof(direction)} not available.")
+                _ => Result.Fail<Coordinates>($"Direction {nameof(direction)} not available for type: {nameof(IRobot)}")
             };
 
-            return result.Success ? 
-                Result.Ok(result.Value) : 
-                Result.Fail<Coordinates>($"Invalid coordinates for movement, cause: {result.Error}");
+            return result.Success
+                ? Result.Ok(result.Value)
+                : Result.Fail<Coordinates>($"Invalid coordinates for movement, cause: {result.Error}");
         }
     }
 }
